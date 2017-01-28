e013f30e57083aa3a5865f5fa927a50ea52356e1
matt <matt@thefowlergroup.com>
2017-01-28 13:35:16 -0600

    ch4 - move static content from views to controllers

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,5 +1,12 @@
 // GET about page.
 
 module.exports.about = function(req, res){
-	res.render('generic-text', { title: 'About' });
+	res.render('generic-text', { 
+		title: 'About',
+		copy: [
+		  'Loc8r was created to help people find places to sit down and git a bit of work done.',
+		  'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.',
+		  'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+		]
+});
 };
