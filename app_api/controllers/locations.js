2485be53ede45a61b5d99b349abb87187a1bb279
matt <matt@thefowlergroup.com>
2017-02-01 23:12:52 -0600

    3.3: create controllers for get single location and review with error handling

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,23 +1,40 @@
 var mongoose = require('mongoose');
 var Loc = mongoose.model('Location');
 
-var sendJsonRequest = function(res, status, content) {
+var sendJsonResponse = function(res, status, content) {
 	res.status(status);
 	res.json(content);
 }
 
 module.exports.locationsListByDistance = function (req, res) {
-	sendJsonRequest(res, 200, {"status" : "success"});
+	sendJsonResponse(res, 200, {"status" : "success"});
 };
 module.exports.locationsCreate = function (req, res) {
-	sendJsonRequest(res, 200, {"status" : "success"});
+	sendJsonResponse(res, 200, {"status" : "success"});
 };
+
 module.exports.locationsReadOne = function (req, res) {
-	sendJsonRequest(res, 200, {"status" : "success"});
+	if (req.params && req.params.locationid) {
+		Loc
+		  .findById(req.params.locationid)
+		  .exec(function(err, location) {
+		  	if (!location) {
+		  		sendJsonResponse(res, 404, { "message" : "locationid not found" });
+		  		return;
+		  	} else if (err) {
+		  		sendJsonResponse(res, 404, err);
+		  		return;
+		  	}
+		  	sendJsonResponse(res, 200, location);
+		  });
+	} else {
+		sendJsonResponse(res, 404, { "message" : "No locationid in request" });
+	}
 };
+
 module.exports.locationsUpdateOne = function (req, res) {
-	sendJsonRequest(res, 200, {"status" : "success"});
+	sendJsonResponse(res, 200, {"status" : "success"});
 };
 module.exports.locationsDeleteOne = function (req, res) {
-	sendJsonRequest(res, 200, {"status" : "success"});
+	sendJsonResponse(res, 200, {"status" : "success"});
 };
